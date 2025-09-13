2658c9800c46b685c093ecfb06194724cb224927
MinhazDeveloper <minhazt14@gmail.com>
2025-09-13 22:13:00 +0600

    Create migration

--- database/migrations/2025_09_13_151103_create_transactions_table.php
+++ database/migrations/2025_09_13_151103_create_transactions_table.php
@@ -13,7 +13,26 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->string('reference')->nullable();
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('account_id');
+            $table->unsignedBigInteger('transfer_id')->nullable(); // কোনো transfer এর সাথে লিঙ্কড থাকলে
+
+            $table->decimal('amount', 16, 4);
+            $table->decimal('balance', 16, 4);
+
+            $table->string('category')->nullable(); // যেমন: deposit, withdrawal, transfer ইত্যাদি
+            $table->boolean('confirmed')->default('0');
+            $table->string('description')->nullable();
+            $table->dateTime('date');
+            $table->text('metal')->nullable();
             $table->timestamps();
+
+            // foreign keys
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
+            $table->foreign('transfer_id')->references('id')->on('transactions')->onDelete('set null');
         });
     }
 
